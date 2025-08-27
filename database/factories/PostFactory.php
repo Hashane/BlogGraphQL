849ec3eed9dc2bb8b34e93a4d30e897dd2a800ae
Hashane <jhashane@gmail.com>
2025-08-28 02:59:33 +0300

    Implementation: Added New Post Model & migration. - Defined relationships.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'    => User::factory(),
+            'title'      => $this->faker->words(4, true),
+            'body'       => $this->faker->paragraph(),
+            'created_at' => $this->faker->dateTimeBetween('-3 months', now()),
+            'updated_at' => now(),
+        ];
+    }
+}
